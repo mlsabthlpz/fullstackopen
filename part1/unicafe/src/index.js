14ca2d1352ed41733d6b3696d1f528c7e7c420e9
melissa.beth.lopez@gmail.com <melissa.beth.lopez@gmail.com>
2020-06-22 16:25:35 -0400

    Exercise 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,19 +3,39 @@ import ReactDOM from 'react-dom'
 
 const Header = ({header}) => <h1>{header}</h1>
 
-const Button = ({handleClick, text}) => (
+const Button = ({handleClick, sent}) => {
+  return (
     <button onClick={handleClick}>
-    {text}
+    {sent.name}
     </button>
     )
+}
 
-const Stats = ({label, count}) => <p>{label} {count}</p>
+const Count = ({sent}) => <p>{sent.name} {sent.count}</p>
+
+const Stats = ({data}) => {
+    if (data.length === 0) {
+        return (<p>Cannot calculate stats until ratings have been submitted.</p>)
+    }
+                
+    const total = data.length
+    const avg = data.reduce((a,b) => a + b)/total
+    const percentpos = (data.filter(x => x===1).length)/total*100
+    return(
+      <div>
+        <p>All {total}</p>
+        <p>Average {(avg)}</p>
+        <p>Positive {(percentpos)}%</p>
+      </div>
+    )
+}
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allSents, setAll] = useState([])
   
   // header names
   const headers = {
@@ -24,25 +44,39 @@ const App = () => {
   }
   
   //labels
-  const labels = {
-      good:'Good',
-      neutral:'Neutral',
-      bad:'Bad'
+  const sentiments = {
+      good:{
+        count: good,
+        name: 'Good',
+        val: 1},
+      neutral:{
+        count: neutral,
+        name: 'Neutral',
+        val: 0},
+      bad:{
+        count: bad,
+        name: 'Bad',
+        val: -1},
   }
   
-  //Add one to the count for a given label
-  const addOne = (label, setLabel) => () => setLabel(label + 1) 
+  //Add one to the count for a given label, add value of sentiment to 
+  const addOne = (label, setLabel, value) => () => {
+      setLabel(label + 1)
+      setAll(allSents.concat(value))
+      console.log(allSents)
+  }
 
   return (
     <div>
       <Header header={headers.feedback} />
-      <Button handleClick={addOne(good, setGood)} text={labels.good} />
-      <Button handleClick={addOne(neutral, setNeutral)} text={labels.neutral} />
-      <Button handleClick={addOne(bad, setBad)} text={labels.bad} />
+      <Button handleClick={addOne(good, setGood, sentiments.good.val)} sent={sentiments.good} />
+      <Button handleClick={addOne(neutral, setNeutral, sentiments.neutral.val)} sent={sentiments.neutral} />
+      <Button handleClick={addOne(bad, setBad, sentiments.bad.val)} sent={sentiments.bad} />
       <Header header={headers.stats} />
-      <Stats label={labels.good} count={good} />
-      <Stats label={labels.neutral} count={neutral} />
-      <Stats label={labels.bad} count={bad} />
+      <Count sent={sentiments.good} />
+      <Count sent={sentiments.neutral}/>
+      <Count sent={sentiments.bad}/>
+      <Stats data={allSents} />
     </div>
   )
 }
