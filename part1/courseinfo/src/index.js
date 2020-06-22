499ef98a8e82c6619c983e34ca4a996673c77e25
melissa.beth.lopez@gmail.com <melissa.beth.lopez@gmail.com>
2020-06-22 10:31:17 -0400

    Exercise 1.3

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -14,7 +14,6 @@ const Part = (props) => {
   }
   
 const Content = (props) => {
-  
   return (
     <div>
       <Part part={props.names[0]} exercise={props.exercises[0]}/>
@@ -32,18 +31,28 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  const names = [part1, part2, part3].map(p => p.name)
+  const exercises = [part1, part2, part3].map(p => p.exercises)
+  const sum = (a, b) => a + b
 
   return (
     <div>
       <Header course={course} />
-      <Content names={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]}/>
-      <Total exercisetotal={exercises1 + exercises2 + exercises3}/>
+      <Content names={names} 
+       exercises={exercises}/>
+      <Total exercisetotal={exercises.reduce(sum)}/>
     </div>
   )
 }
