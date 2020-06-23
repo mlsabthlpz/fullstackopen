1b95e194fe4b3690d90b3a6327544a559ba80735
melissa.beth.lopez@gmail.com <melissa.beth.lopez@gmail.com>
2020-06-23 16:01:38 -0400

    Exercise 2.5

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Total = ({ course }) => {
+  const sum = course.parts.map(part => part.exercises).reduce((a,b)=> a + b)
+  return(
+    <p>Total Number of Exercises {sum}</p>
+  ) 
+}
+
+const Part = ({part}) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>    
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map(part =>
+        <Part key={part.id} part={part} />)
+      }
+    </div>
+  )
+}
+
+const Course = ({course}) => {
+    return (
+      <div>
+        <Header course={course} />
+        <Content course={course} />
+        <Total course={course} />
+      </div>
+    )
+}
+
+export default Course
